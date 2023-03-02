3c0839a8bbd338ce3baf7a815535ebadeb9590f7
guan2737694936 <115344672+guan2737694936@users.noreply.github.com>
2023-03-02 16:05:37 +0800

    Update ceshi.java

--- src/main/java/com/gxa/modules/sys/test/ceshi.java
+++ src/main/java/com/gxa/modules/sys/test/ceshi.java
@@ -3,6 +3,6 @@ package com.gxa.modules.sys.test;
 public class ceshi {
     public static void main(String[] args) {
         System.out.println("1111111");
-        System.out.println("1111111");
+        System.out.println("8888");
     }
 }
